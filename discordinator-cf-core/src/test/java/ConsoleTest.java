a7c01d418ac39748185ca2e33f9da038ebfc4f93
Kevin Xiao <xlzqwerty1@gmail.com>
2016-06-23 20:46:55 -0400

    slightly rewritten message validation and string splitting

--- discordinator-cf-core/src/test/java/ConsoleTest.java
+++ discordinator-cf-core/src/test/java/ConsoleTest.java
@@ -11,7 +11,6 @@ import org.junit.runners.MethodSorters;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
