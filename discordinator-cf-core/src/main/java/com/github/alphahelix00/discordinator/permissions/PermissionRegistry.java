e3946285358ddedd9b71945974c480ac205d740f
Kevin Xiao <xlzqwerty1@gmail.com>
2016-06-26 02:39:09 -0400

    removed cf-core module (now considered legacy)

--- discordinator-cf-core/src/main/java/com/github/alphahelix00/discordinator/permissions/PermissionRegistry.java
+++
@@ -1,8 +0,0 @@
-package com.github.alphahelix00.discordinator.permissions;
-
-/**
- * Created on:   6/15/2016
- * Author:       Kevin Xiao (github.com/alphahelix00)
- */
-public class PermissionRegistry {
-}
